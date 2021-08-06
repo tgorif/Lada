15b8f9fc236eb0a6cf2bed882a361e948cecde32
tgorif <kay.st.janssen@googlemail.com>
2021-08-06 23:09:31 +0200

    Initial commit

---
+++ src/main/java/com/tgorif/Lada/LadaApplication.java
@@ -0,0 +1,13 @@
+package com.tgorif.Lada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgorif/Lada/LadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgorif.Lada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
