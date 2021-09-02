26ac84e2de8a02dcfd3b63f69740800659ed02e7
tgorif <kay.st.janssen@googlemail.com>
2021-09-02 23:29:45 +0200

    i

--- src/main/java/com/tgorif/Lada/ConsumableManager.java
+++ src/main/java/com/tgorif/Lada/ConsumableManager.java
@@ -3,17 +3,32 @@ package com.tgorif.Lada;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-@Component
+
+@Controller
 public class ConsumableManager {
     @Autowired
+    public ConsumableRepository repository;
+    public WebClient client = WebClient.create("http://localhost:7634/Consumable");
     public static ConsumableRepository consumableRepository;
-    public ConsumableManager(ConsumableRepository consumableRepository){consumableRepository=consumableRepository;};
+    public ConsumableManager(){
+    };
     public Consumable findByName(String name){ return consumableRepository.findByName(name);}
-    public List<Consumable> findAll(){ return consumableRepository.findAll();}
+    @PostConstruct
+    private void initRepository(){
+        consumableRepository=repository;
+    }
+
+    public List<Consumable> findAll(){
+        return consumableRepository.findAll();}
     public Map<String,Integer> getQuantities(){
         Map<String,Integer> map = new HashMap<>();
         for(Consumable c : findAll()){
@@ -25,6 +40,12 @@ public class ConsumableManager {
     public static Consumable put(Consumable consumable){
         consumableRepository.save(consumable);
         return consumable;
+    }
+    @GetMapping
+    public String index(Model model){
+        System.out.println("dsad" + repository.findAll().size());
+        model.addAllAttributes(repository.findAll());
+        return "Consumable";
     }
 
 
